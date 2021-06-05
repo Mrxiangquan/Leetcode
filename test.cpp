fa769ddddafde9e72585ad8612d236779d81b51f
Mrxiangquan <13297949626@163.com>
2021-06-05 16:08:55 +0800

    test

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include "iostream"
+using namespace std;
+
+int main(){
+    int a, b;
+    cout << "input a and b" << endl;
+    cin >> a >> b;
+    cout << "a+b is " << a + b << endl;
+    //cout<<"hello world"<<endl;
+    system("pause");
+    return 0;
+}
